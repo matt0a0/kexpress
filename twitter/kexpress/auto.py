baa31db18b02b8951b15be753b56bd58629eb3d4
Matheus Vicente <115947895+veecstart@users.noreply.github.com>
2022-12-15 15:18:06 -0300

    MPostagem - Conectado ao BD  e com Timer

--- twitter/kexpress/auto.py
+++ twitter/kexpress/auto.py
@@ -1,16 +1,40 @@
 from turtle import Screen
 import tweepy
+from pymongo import MongoClient
+import time
 
-consumer_key = "iMjbCfuHuqFbMevNdL9SljJ8c"
-consumer_secret ="PklMYc6j443Tpttk3ZP01jLo1UK8L8wHeb3CI9HM50vo1ArMUD"
-access_token ="1578404739582201857-bnCMYDmPJeOD1GOQBT49Cij6gPkR3j"
-access_token_secret ="CZjbz0YfLM1c1KcQpSdRk45x9ciMmMu3ra4Hj3PY1Lj91"
+CONSUMER_KEY = "8yFjTO9EjWUU8iNgWuajCa8qh"
+CONSUMER_SECRET ="JghQZHFxYHGyrxxIk0FPSCikrWgzS8O4NAd7NjmNpTSSpgOxeZ"
+ACCESS_TOKEN ="1583178044365279239-jd1Sj7iPpWVkYlr3RUCGJHoSfixjDP"
+ACCESS_TOKEN_SECRET ="WThUszj4dtR5kqp3wtxnEehue17jAPbelZAdvwqdIfXPY"
 
 client = tweepy.Client(
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
 )
 
-tweet = client.create_tweet(text="Oi pessoal")
+#Conexão entre o mongodb e o app
+clientMongo = MongoClient("mongodb+srv://ma90theus:<senha de administrador>@cluster0.zkhtqwe.mongodb.net/?retryWrites=true&w=majority")
+#Acessando meu db específico
+db = clientMongo['Tweets']
+#Acessando todos os documentos presentes no meu Collum
+coll = db['tweetGerado']
+
+#Selecionando todos o que estão com a publicação validada
+for i in coll.find({"autorizado" : True},{}):
+    publi = i['texto_postagem']
+    #Aplico o Looped Time
+    #Set do temporizador da publicação
+    begin_clock = time.time()
+    clock_timer = 60
+    while True:
+        current_clock = time.time()
+        past_clock = current_clock - begin_clock
+        if past_clock > clock_timer:
+            print("O sistema de postagem foi iniciado\n ----------Tempo para a próxima postagem{}".format(clock_timer))
+            #Realizo a postagem
+            tweet = client.create_tweet(text=publi)
+            print("###O tweet :  {} foi publicado!".format(tweet))
+            break 
