8d4e997d406dc19cf7f283eac68dbbb096f484ca
Matheus Vicente <115947895+veecstart@users.noreply.github.com>
2022-12-16 09:36:37 -0300

    MGT - Update Manual estados e anos

--- teste_flask/app/commands/postCommands.py
+++ teste_flask/app/commands/postCommands.py
@@ -17,14 +17,23 @@ def buscar_Post(name):
 @click.argument('name')
 def add_Post(name):
     postCollection = mongo.db.posts
+    select_estado = input('Insira a sigla do Estado[AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO]').upper()
+    anos = int(input('Qual a margem de anos que quer analisado (2007 - 2018)'))
     id_imagem =  int(input("Id da imagem a ser postada: "))
     auth = bool(input("Postagem[True/False]: "))
-    texto_postagem = input("Texto a ser Postado : ")
+    texto_select = int(input('Escolha um dos modelos de texto[1/2] :'))
+    if texto_select == 1:
+        texto_postagem = str().format(select_estado,anos)
+    else:
+        texto_postagem = str('O Estado {},tem pouco acesso a educação a quantidade faltosa acesso á escolas de ensino básico foi um dos piores nos ultimos {} anos:').format(select_estado,anos)
     post = {
         "idPost" : name,
+        "select_estado" : select_estado,
+        'anos ' : anos,
         "id_imagem" : id_imagem,
         "autorizado" : auth,
-        "texto_postagem" : texto_postagem
+        "texto_postagem" : texto_postagem,
+        "already_posted" : False
     }
 
     postExists = postCollection.find_one({"idPost" : name})
